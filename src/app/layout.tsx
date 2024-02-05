bc3ee48808fe11aa2cac7c547fd57c777c84c9f2
in-ch <sxin2949@naver.com>
2024-02-05 23:38:30 +0900

    feat: main layout initialize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,25 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-
 import './globals.css';
+import { Inter } from 'next/font/google';
+import Providers from '@/components/Common/Provider';
+import MobileFirstLayout from '@/components/Common/MobileFirstLayout';
+import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: '',
-  description: '',
+export const metadata = {
+  title: '개발 중',
+  description: '개발 중',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
-      <body className={inter.className}>{children}</body>
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <MobileFirstLayout>{children}</MobileFirstLayout>
+        </Providers>
+        <Footer />
+      </body>
     </html>
   );
 }
