30e977db3c710cc0ee277983afe4145c12e8df87
in-ch <sxin2949@naver.com>
2024-03-18 23:54:19 +0900

    style: 모바일 메뉴 배경색상 변경

--- src/components/header/hamburger.tsx
+++ src/components/header/hamburger.tsx
@@ -20,16 +20,17 @@ const Hamburger = (): JSX.Element => {
     <section>
       <RxHamburgerMenu className="md:hidden block" onClick={() => headerShowChange(true)} />
       <div
-        className={`${show ? 'w-screen' : 'w-[0px]'} h-screen z-50 fixed top-0 left-0 bg-red-500 overflow-hidden transition-all duration-500 ease-in-out`}
+        className={`${show ? 'w-screen' : 'w-[0px]'} h-screen z-50 fixed top-0 left-0 overflow-hidden transition-all duration-500 ease-in-out`}
       >
+        <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-b from-white via-sky-200 to-sky-300 backdrop-blur-xl opacity-95 -z-10"></div>
         <div className="flex justify-end">
           <IoIosClose className="w-[50px] h-[50px] mt-1" onClick={() => headerShowChange(false)} />
         </div>
         <ul className="flex flex-col text-2xl gap-6 ml-6">
           <li>home</li>
           <li>about</li>
-          <li>skill</li>
           <li>portfolio</li>
+          <li>blog</li>
           <li>contact</li>
         </ul>
       </div>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,14 +3,14 @@ import Hamburger from './hamburger';
 
 const Header = (): JSX.Element => {
   return (
-    <header className="fixed top-0 left-0 h-[60px] md:h-[70px] w-screen flex flex-row justify-between px-4 md:px-8 items-center">
+    <header className="fixed z-[1000] top-0 left-0 h-[60px] md:h-[70px] w-screen flex flex-row justify-between px-4 md:px-8 items-center">
       <p className="cursor-pointer">in-ch</p>
       <div className="flex flex-row items-center gap-4 cursor-pointer">
         <ul className="hidden flex-row gap-5 md:flex">
           <li>home</li>
           <li>about</li>
-          <li>skill</li>
           <li>portfolio</li>
+          <li>blog</li>
           <li>contact</li>
         </ul>
         <FaGithub className="w-[25px] h-[25px]" />
